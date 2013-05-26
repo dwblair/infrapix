60df27e7158e7d46c42452c8f93273e7449c53e1
Don Blair <donblair>
2013-05-26 11:59:40 -0400

    further broke it

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,9 @@
 import sys, os
 
 # Switch to the virtualenv if we're not already there
-INTERP = os.path.expanduser("~/venv/infrapixenv/bin/python")
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
-sys.path.append(os.getcwd())
+#INTERP = os.path.expanduser("~/venv/infrapixenv/bin/python")
+#if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+#sys.path.append(os.getcwd())
 
 from app import app as application
 
